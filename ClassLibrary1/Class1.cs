5cae073b9eb00d41ed5a8841598a30fed62f5bfc
Tomasz Kopacz <tkopacz@tomaszkopacz.com>
2024-10-15 20:10:37 +0200

    V1

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int Add(int a, int b) => a + b;
+        public int Add3(int a, int b, int c) => Add(a,b) + c;
+        public int Add4(int a, int b, int c, int d) => Add3(a, b, c) + d;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿using ClassLibrary1;
+ClassLibrary1.Class1 c = new();
+Console.WriteLine(c.Add(1,2));
+Console.WriteLine(c.Add3(1, 2, 3));
+Console.WriteLine(c.Add4(1, 2, 3,4));
